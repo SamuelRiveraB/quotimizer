ac2925a51fa05e7d1da5bc8ef7c3597ab1ff10aa
Samuel <samuelrivba@gmail.com>
2024-01-13 22:59:41 -0500

    Add Loading logic

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,14 +4,23 @@ import { FaQuoteLeft } from "react-icons/fa";
 import { FaQuoteRight } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { useEffect, useState} from "react";
+import { LuLoader2 } from "react-icons/lu";
 
 export default function Home() {
+  const [quotes, setQuotes] = useState([])
   const [quote, setQuote] = useState('')
+  const [loading, setLoading] = useState(true)
 
-  function newQuote(quotes) {
-    setQuote(quotes[Math.floor(Math.random() * quotes.length)])
-    if (!quote.author) {
-      quote.author = "Unknown"
+  function newQuote() {
+    setLoading(true)
+    try {
+      setQuote(quotes[Math.floor(Math.random() * quotes.length)])
+      if (quote && !quote.author) {
+        quote.author = "Unknown"
+      }
+      setLoading(false)
+    } catch (error) {
+      console.log("Error: ", error)
     }
   }
 
@@ -19,15 +28,21 @@ export default function Home() {
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
       const response = await fetch(apiUrl)
-      const quotes = await response.json()
-      newQuote(quotes)
+      const responseJson = await response.json()
+      setQuotes(responseJson)
     } catch (error) {
       console.log("Error ", error)
     }
   }
 
+  useEffect(() => {
+    if(quotes.length>0) {
+      newQuote()
+    }
+  }, [quotes]);
+
   function tweetQuote() {
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote.text} \n- ${quote.author}`
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote.text} - ${quote.author}`
     window.open(twitterUrl, '_blank')
   }
 
@@ -37,8 +52,9 @@ export default function Home() {
 
   return (
     <main className="h-screen flex flex-col items-center justify-center text-slate-600 text-4xl p-4">
-        <div className="max-w-[900px] rounded-md p-4 bg-white/50 shadow-2xl">
-          <div className="">
+        {loading && (<LuLoader2 id="loader" className="animate-spin absolute"/>)}
+        {!loading && (<div id="container" className="max-w-[900px] rounded-md p-4 bg-white/50 shadow-2xl">
+          <div>
             <FaQuoteLeft className="inline-block mr-1" />
             <span id="quote">{quote.text}</span>
             <FaQuoteRight className="inline-block ml-1"/>
@@ -50,9 +66,9 @@ export default function Home() {
             <button onClick={tweetQuote} title="Tweet This!">
               <FaSquareXTwitter className="text-slate-900 hover:text-slate-700 text-6xl"/>
             </button>
-            <button className="p-4 bg-slate-900 hover:bg-slate-700 text-white text-xl rounded-xl shadow-xl">New Quote</button>
+            <button onClick={newQuote}className="p-4 bg-slate-900 hover:bg-slate-700 text-white text-xl rounded-xl shadow-xl">New Quote</button>
           </div>
-        </div>
+        </div>)}
     </main>
   )
 }
